8220b7efd9a894266dce8f4ce9ab23e714b4e032
Raitis Saripo <saripo.raitis@gmail.com>
2021-02-16 20:06:59 +0200

    Initial Commit

---
+++ GamerApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GamerApp
+//
+//  Created by Raitis Saripo on 16/02/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
